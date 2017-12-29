24f7a592a10d41cac49f1bf60ef55f9c46f6440e
Anton <31567808+Anton-Khan@users.noreply.github.com>
2017-12-29 12:16:01 +0400

    Add files via upload

---
+++ enter.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N = 10;
+	
+	int arr[N];
+	
+	for(int i = 0; i<N; i++)
+	{
+		cout << "Введите значение для " << i+1 << " элемента массива:";
+		while(!(cin >> arr[i]))
+		{
+			cin.clear();
+			while(cin.get() != '\n');
+			
+			cout << "Введите значение для " << i+1 << " элемента массива:";
+		}
+	}
+	
+	for(int i=0; i<N; i++)
+	cout << arr[i] << endl;
+	
+	
+	_getch();
+	return 0;
+}
